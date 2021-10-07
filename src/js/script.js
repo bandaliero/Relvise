6def6dda2b4fb93b02f70928abf7306b774a7766
bandaliero <bandaliero@gmail.com>
2021-10-07 17:28:28 +0300

    header section

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ let links = document.querySelector('.nav-links')
 let menu = document.querySelector('.nav-menu')
 let navBg = document.querySelector('.header-nav')
 let rootBg = document.querySelector('.root-bg_switcher')
+let logo = document.querySelector('.nav-logo_link')
 
 menu.addEventListener('click', function () {
     links.classList.toggle('active')
@@ -9,12 +10,18 @@ menu.addEventListener('click', function () {
 })
 
 window.addEventListener('scroll', function () {
+
     navBg.classList.add('activeBg')
+    // logo.style.color = '#000';
+
     let check = window.scrollY
 
     if(check <= 0) {
         navBg.classList.remove('activeBg')
+        // logo.style.color = '#fff'
     }
+
+
 })
 
 

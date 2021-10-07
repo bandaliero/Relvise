9515707ae8b2214be0e53e4a647f01252a6b53e9
bandaliero <bandaliero@gmail.com>
2021-10-07 11:31:33 +0300

    header-nav

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,20 @@
-let menu = document.getElementById('burger');
-let links = document.getElementById('links');
-let linkStyle = getComputedStyle(links);
+let links = document.querySelector('.nav-links')
+let menu = document.querySelector('.nav-menu')
+let navBg = document.querySelector('.header-nav')
+let rootBg = document.querySelector('.root-bg_switcher')
 
-menu.addEventListener("click", function () {
-    if(linkStyle.display === 'none'){
-        links.classList.add('active')
-    } else {
-        links.classList.remove('active')
+menu.addEventListener('click', function () {
+    links.classList.toggle('active')
+    rootBg.classList.toggle('activeDisplay')
+})
+
+window.addEventListener('scroll', function () {
+    navBg.classList.add('activeBg')
+    let check = window.scrollY
+
+    if(check <= 0) {
+        navBg.classList.remove('activeBg')
     }
 })
+
+
